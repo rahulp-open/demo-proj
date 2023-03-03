68f777feec995647beb9cce3ad3868c47a7a3fda
Ed Preston <epreston@prestonsoft.com>
2023-03-04 07:30:59 +1100

    Update main.ts
    Build off of source instead of the bundled build.  This allows you to see the effects of the changes and experiment.  The results show up when you build.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './style.css'
 
-import * as THREE from "three";
+// @ts-ignore
+import * as THREE from "../node_modules/three/src/Three.js";
 const scene = new THREE.Scene();
 
 console.log(scene);
+
